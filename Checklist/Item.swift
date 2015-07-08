de40f3ea622c863d7d252578d4ba4ea1c2aae8e0
Remus Lazar <remus@lazar.info>
2015-07-08 18:00:54 +0200

    SortableMO super class

--- Checklist/Item.swift
+++ Checklist/Item.swift
@@ -9,32 +9,8 @@
 import Foundation
 import CoreData
 
-class Item: NSManagedObject {
+class Item: SortableMO {
 
     @NSManaged var title: String
-    @NSManaged var sorting: NSNumber
-    @NSManaged var timestamp: NSDate
     
-    override func awakeFromInsert() {
-        timestamp = NSDate()
-        if let maxSorting = aggr("max:", forKeyPath: "sorting", attributeType: .Integer32AttributeType) as? Int {
-            sorting = maxSorting + 1
-        }
-    }
-    
-    // helper method to call a aggr function like max, min etc.
-    private func aggr(function: String, forKeyPath keyPath: String, attributeType: NSAttributeType) -> AnyObject? {
-        let description = NSExpressionDescription()
-        description.name = "a"
-        description.expression = NSExpression(forFunction: function, arguments: [NSExpression(forKeyPath: keyPath)])
-        description.expressionResultType = attributeType
-
-        let request = NSFetchRequest(entityName: entity.name!)
-        request.resultType = .DictionaryResultType
-        request.propertiesToFetch = [description]
-        
-        return managedObjectContext?.executeFetchRequest(request, error: nil)?.last?[description.name]
-    }
-    
-
 }
